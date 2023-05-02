156995f55c6f03bca7110fb2057b242fa7becd56
Sandy H <sandyharyono@gmail.com>
2023-05-02 08:02:33 +0700

    initial files

---
+++ src/main/java/com/thales/id/jakarta/entities/response/DeTokenizeResponse.java
@@ -0,0 +1,15 @@
+package com.thales.id.jakarta.entities.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class DeTokenizeResponse {
+    private String data;
+    private String status;
+
+
+}
